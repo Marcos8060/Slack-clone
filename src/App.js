b0740986100a993b244c38d6ded10f0f7cbe94fd
Marcos8060 <marcosgav80@gmail.com>
2021-12-29 17:53:23 +0300

    create login and message component

--- src/App.js
+++ src/App.js
@@ -1,27 +1,38 @@
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import Chat from './Chat';
+import { useState } from 'react';
 
 function App() {
+  const [user, setUser] = useState(null);
+
   return (
     <div className="app">
-      <Router> 
-        <Header />
-        <div className="app_body">
-          <Sidebar />
-          <Switch>
-            <Route path="/room/:roomId">
-              <Chat />
-            </Route>
-            <Route path='/'>
-              <h1>Welcome</h1>
-            </Route>
-          </Switch>
-        </div>
+      <Router>
+        {!user ? (
+          <h1>Login here</h1>
+        ) : (
+          <>
+            <Header />
+            <div className="app_body">
+              <Sidebar />
+              <Switch>
+                <Route path="/room/:roomId">
+                  <Chat />
+                </Route>
+                <Route path='/'>
+                  <h1>Welcome</h1>
+                </Route>
+              </Switch>
+            </div>
+          </>
+
+        )}
+
       </Router>
-      
+
     </div>
   );
 }
--- src/Chat.js
+++ src/Chat.js
@@ -4,10 +4,12 @@ import {useParams } from 'react-router-dom'
 import StarOutlineIcon from '@mui/icons-material/StarOutline';
 import InfoIcon from '@mui/icons-material/Info';
 import db from './firebase';
+import Message from './Message';
 
 function Chat() {
     const {roomId} = useParams();
-    const [roomDetails,setRoomDetails] = useState();
+    const [roomDetails,setRoomDetails] = useState(null);
+    const [roomMessages, setRoomMessages] = useState([]);
 
     useEffect(()=>{
         if(roomId){
@@ -15,7 +17,12 @@ function Chat() {
                setRoomDetails(snapshot.data())
            ))
         }
+
+        db.collection('rooms').doc(roomId).collection('messages').orderBy('timestamp','asc')
+        .onSnapshot(snapshot => setRoomMessages(snapshot.docs.map(doc => doc.data())))
     },[roomId])
+
+    console.log(roomMessages);
 
     return (
         <div className='chat'>
@@ -31,6 +38,19 @@ function Chat() {
                         <InfoIcon /> Details
                     </p>
                 </div>
+            </div>
+            <div className="chat_messages">
+                {roomMessages.map((message)=>{
+                    return (
+                    <Message 
+                      user={message.user}
+                      message={message.message}
+                      userimage={message.userimage}
+                      timestamp={message.timestamp}
+                      key={message.id}
+                    />
+                    )
+                })}
             </div>
         </div>
     )
